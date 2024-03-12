2cb3f2347ce18e2157e62679a627ba0b7539e526
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-12 17:17:21 -0400

    just stuff about strings

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print('hello world')
+print('mike')    
+
+long_string = '''
+what up
+that's amazing
+see that
+
+'''
+
+print(long_string)
+
+# escape sequences
+weather ="\'98 degrees \"wow its hot\""
+# \t  = tab
+# \n  = new line
+print(weather)
+# formatted strings
+print(f'formatted string for weather: {weather}')
