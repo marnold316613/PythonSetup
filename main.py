fcb78889e303042dbb30145de5f94eddd40c5455
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-13 09:38:29 -0400

    just a little more string theory

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ weather ="\'98 degrees \"wow its hot\""
 print(weather)
 # formatted strings
 print(f'formatted string for weather: {weather}')
+# strings are just array
+print(weather[0:6:1])  # start and stop and step over
+print(weather[::2])
+print(weather[::-1])  # - means start from the end and work backwards
+                      # strings are immutable, you can't get do weather[0]='some value'
+print('length',len(weather))
+print(weather.upper())
