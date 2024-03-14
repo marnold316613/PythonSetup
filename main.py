6443dc36caf4bf3c40a8468710ccd88cbd794a1b
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-14 08:13:39 -0400

    messing around with dates

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-print('hello world')
-print('mike')    
+# print('hello world')
+# print('mike')    
 
-long_string = '''
-what up
-that's amazing
-see that
+# long_string = '''
+# what up
+# that's amazing
+# see that
 
-'''
+# '''
 
-print(long_string)
+# print(long_string)
 
-# escape sequences
-weather ="\'98 degrees \"wow its hot\""
-# \t  = tab
-# \n  = new line
-print(weather)
-# formatted strings
-print(f'formatted string for weather: {weather}')
-# strings are just array
-print(weather[0:6:1])  # start and stop and step over
-print(weather[::2])
-print(weather[::-1])  # - means start from the end and work backwards
-                      # strings are immutable, you can't get do weather[0]='some value'
-print('length',len(weather))
-print(weather.upper())
+# # escape sequences
+# weather ="\'98 degrees \"wow its hot\""
+# # \t  = tab
+# # \n  = new line
+# print(weather)
+# # formatted strings
+# print(f'formatted string for weather: {weather}')
+# # strings are just array
+# print(weather[0:6:1])  # start and stop and step over
+# print(weather[::2])
+# print(weather[::-1])  # - means start from the end and work backwards
+#                       # strings are immutable, you can't get do weather[0]='some value'
+# print('length',len(weather))
+# print(weather.upper())
+from datetime import datetime
+
+
+birth_year=input('What year were you born?')
+
+mydate= datetime.today()
+
+print(mydate)
+
+print(datetime(int(birth_year),1,1))
+delta = mydate- datetime(int(birth_year),1,1)
+
+age = (int(delta.days/365.25))
+
+
+print(f"You are {age}")
