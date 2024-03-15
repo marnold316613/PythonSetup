32ed0dce254b9ca492a7003ccd89e40ebbe59235
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-15 14:51:21 -0400

    learning about lists

--- main.py
+++ main.py
@@ -27,16 +27,27 @@
 from datetime import datetime
 
 
-birth_year=input('What year were you born?')
+# birth_year=input('What year were you born?')
 
-mydate= datetime.today()
+# mydate= datetime.today()
 
-print(mydate)
+# print(mydate)
 
-print(datetime(int(birth_year),1,1))
-delta = mydate- datetime(int(birth_year),1,1)
+# print(datetime(int(birth_year),1,1))
+# delta = mydate- datetime(int(birth_year),1,1)
 
-age = (int(delta.days/365.25))
+# age = (int(delta.days/365.25))
 
 
-print(f"You are {age}")
+# print(f"You are {age}")
+
+# repeatcharacter='*'* 10  # this is an easy way to just repeat a character thru multiplication
+
+# print('repeat chararcters', repeatcharacter)
+
+# lists can be used like javascript arrays
+li=[1,2,3,'a','b','c', True, False]
+
+print(li)
+print(li[2])
+print(len(li))
