64b4d67adfc2669298434da6249bf0a8ff5527aa
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-03-31 15:25:06 -0400

    working on classes

--- myobjects.py
+++ myobjects.py
@@ -1,1 +1,11 @@
-print('hello world')
+class PlayerCharacter:
+  #class object attribute, its a constant
+  membership=True
+  def __init__(self, name):
+    self.name = name
+  
+  def run(self):
+    print(f'run {self.name} the zombie is coming')
+
+player1 = PlayerCharacter('mike')
+player1.run()
